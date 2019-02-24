5520c1bad0abe0e9a3897badf52333d9ce6fafa2
Hallan Medeiros <hmhallan@gmail.com>
2019-02-24 11:00:04 -0300

    ajuste para usar o DAO do framework

--- src/main/java/com/hallanmedeiros/exp2/data/IrisAlgorithm.java
+++ src/main/java/com/hallanmedeiros/exp2/data/IrisAlgorithm.java
@@ -1,17 +1,15 @@
 package com.hallanmedeiros.exp2.data;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 import machineLearning.Algorithm;
 import weka.classifiers.AbstractClassifier;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
-import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.converters.CSVLoader;
 
 public class IrisAlgorithm extends Algorithm {
 
@@ -20,56 +18,41 @@ public class IrisAlgorithm extends Algorithm {
 
 	@Override
 	public boolean fit(double[][] features, double[] labels) {
-
-//		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
-//		attributes.add(new Attribute("w"));
-//		attributes.add(new Attribute("x"));
-//		attributes.add(new Attribute("y"));
-//		attributes.add(new Attribute("z"));
-//		attributes.add(new Attribute("class", new FastVector<>(Attribute.NOMINAL)));
-//		
-//		this.instances = new Instances("dataset", attributes, 150);
-//		this.instances.setClassIndex(4);
-//		
-//		for (int i = 0; i < features.length-1; i++) {
-//				Instance novo = new DenseInstance(5);
-//				novo.setDataset(this.instances);
-//				
-//				System.out.println(Arrays.toString(features[i]));
-//				System.out.println(LabelsIris.values()[(int)labels[i]].name());
-//				
-//		        novo.setValue(0, features[i][0]);
-//		        novo.setValue(1, features[i][1]);
-//		        novo.setValue(2, features[i][2]);
-//		        novo.setValue(3, features[i][3]);
-//		        novo.setValue(4, labels[i] );
-////		        novo.setValue(4, LabelsIris.values()[(int)labels[i]].name() );
-//		        
-//		        this.instances.add(novo);
-//		}		
-//		
-//		this.classifier = new NaiveBayes();
-//		try {
-//			this.classifier.buildClassifier(this.instances);
-//		} catch (Exception e) {
-//			throw new RuntimeException(e);
-//		}
 		
-		CSVLoader csv = new CSVLoader();
-        try {
-            csv.setSource(this.getClass().getResourceAsStream("/iris.csv"));
-            this.instances = csv.getDataSet();
-
-            //indice do atributo que é 'classe'
-            this.instances.setClassIndex(4);
-
-            this.classifier = new NaiveBayes();
-            this.classifier.buildClassifier(this.instances);
-        }
-        catch (Exception ex){
-            ex.printStackTrace();
-        }
+		List<String> tipos = new ArrayList<>();
+		tipos.add("Iris-setosa");
+		tipos.add("Iris-versicolor");
+		tipos.add("Iris-virginica");
 		
+		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+		attributes.add(new Attribute("w"));
+		attributes.add(new Attribute("x"));
+		attributes.add(new Attribute("y"));
+		attributes.add(new Attribute("z"));
+		attributes.add(new Attribute("class", tipos));
+		
+		this.instances = new Instances("dataset", attributes, 5);
+		this.instances.setClassIndex(4);
+		
+		for (int i = 0; i < features.length-1; i++) {
+				Instance novo = new DenseInstance(5);
+				novo.setDataset(this.instances);
+				
+		        novo.setValue(0, features[i][0]);
+		        novo.setValue(1, features[i][1]);
+		        novo.setValue(2, features[i][2]);
+		        novo.setValue(3, features[i][3]);
+		        novo.setValue(4, labels[i] );
+		        
+		        this.instances.add(novo);
+		}		
+		
+		this.classifier = new NaiveBayes();
+		try {
+			this.classifier.buildClassifier(this.instances);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 		
 		return true;             
 	}
